8509e7dedef161c77131a3e20fd0e3ae7880b3a5
oneup <hello@geeq.at>
2009-07-19 21:49:05 +0200

    basic ruby repl online

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -2,8 +2,30 @@ class ConsoleController < ApplicationController
   def index
   end
 
-  def do
-    flash[:notice] = "The flash system is really helpful but kinda borked. Also: #{params[:line]}"
+  def do    
+    code = params[:line]
+    
+    
+    # do something good,
+    # save it when worked. build online hax repository
+    
+    # actually this is quite dangerous un-sandboxed,
+    # on a webservice. means passwords are obsolete here.
+    
+    begin
+      message = Kernel.eval(code)
+    rescue Exception => e
+      "Exception " + e.to_s
+    rescue SyntaxError => e
+      "Syntax Error #{e}"
+    rescue Error => e
+      "Error #{e}"
+    rescue NameError => e #"multicast" nameerror, so inventory can caputre it
+      "NameError #{e}"
+    end
+    
+    flash[:notice] = "flash: #{params[:line]} => #{message}"
+    
     redirect_to :action => :index
   end
 
