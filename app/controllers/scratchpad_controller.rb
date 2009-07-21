b967caf9275c326e3789475eba3779f5655f630d
oneup <hello@geeq.at>
2009-07-21 02:17:20 +0200

    deleted tutorial, cleansed sidebar

--- app/controllers/scratchpad_controller.rb
+++ app/controllers/scratchpad_controller.rb
@@ -4,9 +4,13 @@ class ScratchpadController < ApplicationController
   end
   
   def start # first landing page for new/registered users
-    redirect_to :action => :tutorial
-  end
+    name = "root"
+    p = Page.find_by_name name
 
-  def tutorial
+    if p
+      redirect_to url_for :controller => :pages, :action => :show, :id => p
+    else
+      redirect_to url_for :controller => :pages, :action => :new
+    end
   end
 end
