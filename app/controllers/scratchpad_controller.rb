07b51d7d3f884417f6f365203467ace940bfc105
oneup <hello@geeq.at>
2009-07-21 02:20:00 +0200

    cleaner intro page eval

--- app/controllers/scratchpad_controller.rb
+++ app/controllers/scratchpad_controller.rb
@@ -4,8 +4,7 @@ class ScratchpadController < ApplicationController
   end
   
   def start # first landing page for new/registered users
-    name = "root"
-    p = Page.find_by_name name
+    p = Page.first
 
     if p
       redirect_to url_for :controller => :pages, :action => :show, :id => p
